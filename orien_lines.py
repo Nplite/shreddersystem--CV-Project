8ba6b041ad2bc52955c653009f3a2b4ecbb16a87
Nplite <76342ns@gmail.com>
2023-03-03 00:57:41 +0530

    Line color changes

--- orien_lines.py
+++ orien_lines.py
@@ -54,8 +54,8 @@ def drawsafelines(image_np,Orientation,Line_Perc1,Line_Perc2):
         
         Line_Position2=int(image_np.shape[1]*(Line_Perc2/100))
         
-        cv2.line(img=image_np, pt1=(Line_Position1, 0), pt2=(Line_Position1,image_np.shape[0]), color=(0, 0, 255), thickness=2, lineType=8, shift=0)
+        cv2.line(img=image_np, pt1=(Line_Position1, 0), pt2=(Line_Position1,image_np.shape[0]), color=(255, 0, 0), thickness=2, lineType=8, shift=0)
                 
-        cv2.line(img=image_np, pt1=(Line_Position2, 0), pt2=(Line_Position2,image_np.shape[0]), color=(255, 0, 0), thickness=2, lineType=8, shift=0)
+        cv2.line(img=image_np, pt1=(Line_Position2, 0), pt2=(Line_Position2,image_np.shape[0]), color=(0, 0, 255), thickness=2, lineType=8, shift=0)
         
         return Line_Position2;
